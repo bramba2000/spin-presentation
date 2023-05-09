5b9cf641aad329fd5f4d8c87ae89b2126c5888d5
Matteo Brambilla <bramba2000@gmail.com>
2023-05-09 11:52:34 +0200

    fix callout component

--- components/Callout.vue
+++ components/Callout.vue
@@ -1,6 +1,29 @@
 <template>
-    <div w="full" border="~ main rounded-md" flex="~" pl="1.5 rem" bg="dark">
-        <div w="30px"><p><slot name="icon"></slot></p></div>
-        <div pl="~ 0.5rem"><slot name="default"></slot></div>
-    </div>
-</template>
+    <div>
+        <LightOrDark>
+            <template #dark>
+                <div w="full" border="~ main rounded-md" flex="~" pl="1.5 rem" bg="dark">
+                    <div w="30px">
+                        <p>
+                            <slot name="icon"></slot>
+                        </p>
+                    </div>
+                    <div pl="~ 0.5rem">
+                        <slot name="default"></slot>
+                    </div>
+                </div>
+            </template>
+            <template #light>
+                <div w="full" border="~ main rounded-md" flex="~" pl="1.5 rem" bg="light">
+                    <div w="30px">
+                        <p>
+                            <slot name="icon"></slot>
+                        </p>
+                    </div>
+                    <div pl="~ 0.5rem">
+                        <slot name="default"></slot>
+                </div>
+            </div>
+        </template>
+    </LightOrDark>
+</div></template>
