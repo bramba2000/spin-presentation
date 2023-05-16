5c05f1674edf2012bf3fde5aa6088500ba9ab11c
Matteo Brambilla <bramba2000@gmail.com>
2023-05-16 11:52:25 +0200

    reconfigure shiki to include all standard lang

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -3,20 +3,23 @@ import shiki from "shiki"
 import { readFileSync } from 'fs'
 
 const myLanguageGrammar = JSON.parse(readFileSync('./promela.tmLanguage.json', 'utf-8'))
+const myLanguage = {
+  id: 'promela',
+  scopeName: 'source.promela',
+  grammar: myLanguageGrammar,
+  aliases: ['promela', 'spin'],
+  path: 'promela.tmLanguage.json',
+}
 
-export default defineShikiSetup(() => {
+
+export default defineShikiSetup(async () => {
+  const highlighter = await shiki.getHighlighter({})
+  highlighter.loadLanguage(myLanguage)
   return {
     theme: {
       dark: 'min-dark',
       light: 'nord',
     },
-    langs: [{
-      id: 'promela',
-      scopeName: 'source.promela',
-      grammar: myLanguageGrammar,
-      aliases: ['promela', 'spin'],
-      path: 'promela.tmLanguage.json',
-    }
-    ],
+    highlighter: highlighter
   }
 })
